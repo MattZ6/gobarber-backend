157e067fe6d4da5d09ccb833003597037bd89933
Matheus Zanin <matt_z6@hotmail.com>
2020-05-06 20:18:13 -0300

    test(Appointment): Create appointments service tests

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -20,7 +21,22 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe(provider_id);
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1).toBe(1);
-  // });
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const date = new Date(2020, 4, 10, 11);
+    const provider_id = 'ahsd98h1';
+
+    await createAppointment.execute({
+      date,
+      provider_id,
+    });
+
+    expect(
+      createAppointment.execute({ date, provider_id })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
